d1137438e4ade7f0d6887a40633c080680712c36
Yuzu-io <1581523609@qq.com>
2023-09-28 11:57:33 +0800

    feat:编写menu组件

---
+++ src/components/Menu/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menu">
+    <t-menu collapsed>
+      <t-menu-item value="0">
+        <template #icon>
+          <t-icon name="app" />
+        </template>
+        仪表盘
+      </t-menu-item>
+
+      <t-menu-item value="1">
+        <template #icon>
+          <t-icon name="image-edit" />
+        </template>
+        <span>人台图</span>
+      </t-menu-item>
+    </t-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  width: 64px;
+  padding: 10px;
+}
+</style>
---
+++ src/views/dressing/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dressing">
+    <Menu></Menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Menu from "@/components/Menu/index.vue";
+</script>
+
+<style lang="scss" scoped>
+.dressing {
+  background-color: #ECEFF5;
+  height: 100vh;
+}
+</style>
