b8fe94f43991f4c7fd944fb6a7c32d4b19724a7e
Yuzu.io <1581523609@qq.com>
2023-10-07 23:22:11 +0800

    feat:添加提示

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -5,26 +5,31 @@ const routes: Array<RouteRecordRaw> = [
     path:'/',
     redirect:'/spell'
   },
+  // 登录
   {
     path: '/login',
     name: 'login',
     component: () => import('@/views/login/index.vue')
   },
+  // 首页
   {
     path:'/home',
     name:'home',
     component:()=>import('@/views/home/index.vue')
   },
+  // 抠图
   {
     path: '/cutout',
     name: 'cutout',
     component: () => import('@/views/cutout/index.vue')
   },
+  // 咒语
   {
     path: '/spell',
     name: 'spell',
     component: () => import('@/views/spell/index.vue')
   },
+  // 炼丹结果
   {
     path: '/achievements',
     name: 'achievements',
